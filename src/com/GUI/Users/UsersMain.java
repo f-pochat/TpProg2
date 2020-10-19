2fdb0105fbb796b62535edbb7eff391dec27d6fe
fpochat02 <fpochat@losmolinos.edu.ar>
2020-10-19 01:02:56 -0300

    Agregue la funcion de agrega al contacto en el archivo Agregue funciones en UserWriterReader para facilitar leer y escribir el archivo. (Leerlo como un arraylist de arraylists) -- IMPORTANTE -- Fijense que agregue una nueva columna el archivo users. Esa tercera columna dice la cantidad de contactos que tiene. Nos va a facilitar leerlos.

--- src/com/GUI/Users/UsersMain.java
+++ src/com/GUI/Users/UsersMain.java
@@ -7,7 +7,10 @@ import java.awt.event.ActionEvent;
 import javax.swing.*;
 
 public class UsersMain extends JFrame {
-    public UsersMain() {initComponents();}
+    public UsersMain() {
+        initComponents();
+
+    }
 
     private void button2ActionPerformed(ActionEvent e) {
         JFrame sintomas = new UsersSintomas();
@@ -33,6 +36,7 @@ public class UsersMain extends JFrame {
         button1 = new JButton();
         button2 = new JButton();
         button3 = new JButton();
+        label1 = new JLabel();
         menu = new JPanel();
 
         //======== this ========
@@ -47,18 +51,17 @@ public class UsersMain extends JFrame {
 
         //---- button1 ----
         button1.setText("Indicar contacto estrecho");
-        button1.addActionListener(e -> {
-			button1ActionPerformed(e);
-		});
+        button1.addActionListener(e -> button1ActionPerformed(e));
 
         //---- button2 ----
         button2.setText("Presencia de sintomas");
-        button2.addActionListener(e -> {
-			button2ActionPerformed(e);
-		});
+        button2.addActionListener(e -> button2ActionPerformed(e));
 
         //---- button3 ----
         button3.setText("Mapa");
+
+        //---- label1 ----
+        label1.setText("Text");
 
         GroupLayout contentPaneLayout = new GroupLayout(contentPane);
         contentPane.setLayout(contentPaneLayout);
@@ -70,35 +73,43 @@ public class UsersMain extends JFrame {
                         .addComponent(button3, GroupLayout.PREFERRED_SIZE, 277, GroupLayout.PREFERRED_SIZE)
                         .addComponent(button2, GroupLayout.PREFERRED_SIZE, 277, GroupLayout.PREFERRED_SIZE)
                         .addComponent(button1, GroupLayout.PREFERRED_SIZE, 277, GroupLayout.PREFERRED_SIZE))
-                    .addContainerGap(147, Short.MAX_VALUE))
+                    .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
-                    .addContainerGap(190, Short.MAX_VALUE)
-                    .addComponent(label5)
-                    .addGap(214, 214, 214))
+                    .addContainerGap(208, Short.MAX_VALUE)
+                    .addGroup(contentPaneLayout.createParallelGroup()
+                        .addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
+                            .addComponent(label1, GroupLayout.PREFERRED_SIZE, 132, GroupLayout.PREFERRED_SIZE)
+                            .addGap(34, 34, 34))
+                        .addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
+                            .addComponent(label5)
+                            .addGap(196, 196, 196))))
         );
         contentPaneLayout.setVerticalGroup(
             contentPaneLayout.createParallelGroup()
                 .addGroup(contentPaneLayout.createSequentialGroup()
-                    .addGap(86, 86, 86)
+                    .addGap(19, 19, 19)
+                    .addComponent(label1)
+                    .addGap(48, 48, 48)
                     .addComponent(label5)
-                    .addGap(49, 49, 49)
+                    .addGap(40, 40, 40)
                     .addComponent(button1)
                     .addGap(18, 18, 18)
                     .addComponent(button2)
                     .addGap(18, 18, 18)
                     .addComponent(button3)
-                    .addContainerGap(146, Short.MAX_VALUE))
+                    .addContainerGap(158, Short.MAX_VALUE))
         );
         pack();
         setLocationRelativeTo(getOwner());
 
         //======== menu ========
         {
-            menu.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border. EmptyBorder(
-            0, 0, 0, 0) , "JF\u006frmDes\u0069gner \u0045valua\u0074ion", javax. swing. border. TitledBorder. CENTER, javax. swing. border. TitledBorder
-            . BOTTOM, new java .awt .Font ("D\u0069alog" ,java .awt .Font .BOLD ,12 ), java. awt. Color.
-            red) ,menu. getBorder( )) ); menu. addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java .
-            beans .PropertyChangeEvent e) {if ("\u0062order" .equals (e .getPropertyName () )) throw new RuntimeException( ); }} );
+            menu.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border.
+            EmptyBorder( 0, 0, 0, 0) , "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn", javax. swing. border. TitledBorder. CENTER, javax. swing
+            . border. TitledBorder. BOTTOM, new java .awt .Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ),
+            java. awt. Color. red) ,menu. getBorder( )) ); menu. addPropertyChangeListener (new java. beans. PropertyChangeListener( )
+            { @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062ord\u0065r" .equals (e .getPropertyName () ))
+            throw new RuntimeException( ); }} );
 
             GroupLayout menuLayout = new GroupLayout(menu);
             menu.setLayout(menuLayout);
@@ -112,6 +123,12 @@ public class UsersMain extends JFrame {
             );
         }
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
+        if (usuario.getTel() != null){
+            label1.setText(String.valueOf("Bienvenido, " + usuario.getTel()));
+        }else{
+            label1.setText("");
+        }
+
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
@@ -120,6 +137,7 @@ public class UsersMain extends JFrame {
     private JButton button1;
     private JButton button2;
     private JButton button3;
+    private JLabel label1;
     private JPanel menu;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
--- src/com/ReadersWriter/UserWriterReader.java
+++ src/com/ReadersWriter/UserWriterReader.java
@@ -4,8 +4,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
 /**
  * FedePochat & SebaAdaro
  */
@@ -15,7 +15,8 @@ public class UserWriterReader {
         try{
             FileWriter fileWriter = new FileWriter("users.txt",true);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write(tel + "," + cuil+"\n");
+            bufferedWriter.write(tel + "," + cuil+ ",0," +"\n");
+            bufferedWriter.flush();
             bufferedWriter.close();
             fileWriter.close();
         }catch (Exception e){
@@ -51,6 +52,21 @@ public class UserWriterReader {
         }
     }
 
+    public static int readNumOfContacts(String tel){
+        try{
+            ArrayList<ArrayList<String>> users = readFile();
+            ArrayList<String> tels = new ArrayList<>();
+            for (List<String> list : users){
+                tels.add(list.get(0));
+            }
+            //Cast de String a Int
+            return Integer.parseInt(users.get(tels.indexOf(tel)).get(2));
+        }catch (Exception e){
+            System.out.println(e);
+            return 0;
+        }
+    }
+
     public static Map readTelandCuil(){
         try {
             FileReader fileReader = new FileReader("users.txt");
@@ -68,4 +84,76 @@ public class UserWriterReader {
             return null;
         }
     }
+
+    //Escribe en la linea de tel el contenido al final
+    public static void addContact(String tel, String content){
+        ArrayList<ArrayList<String>> users = readFile();
+        ArrayList<String> tels = new ArrayList<>();
+        for (List<String> list : users){
+            tels.add(list.get(0));
+        }
+        if (tels.contains(tel)) {
+            ArrayList<String> listDelTel = users.get(tels.indexOf(tel));
+            listDelTel.add(content);
+            int newNumofContacts = Integer.parseInt(listDelTel.get(2))+1;
+            listDelTel.set(2, String.valueOf(newNumofContacts));
+            try{
+                FileWriter fileWriter = new FileWriter("users.txt");
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                for (int i = 0; i < users.size(); i++) {
+                    for (String str: users.get(i)){
+                        bufferedWriter.append(str + ",");
+                    }
+                    bufferedWriter.append("\n");
+                }
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                fileWriter.close();
+            }catch (Exception e){
+                System.out.println(e);
+            }
+        }else{
+            return;
+        }
+    }
+
+    //Lee el archivo y lo devuelve como un arraylist de arraylists
+    private static ArrayList<ArrayList<String>> readFile(){
+        try {
+            FileReader fileReader = new FileReader("users.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            ArrayList<ArrayList<String>> user = new ArrayList<>();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                List<String> content = Arrays.asList(line.split(","));
+                ArrayList<String> cont = new ArrayList<>(content);
+                user.add(cont);
+            }
+            fileReader.close();
+            return user;
+        }catch (Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public static boolean containsContact(String tel, String contact){
+        ArrayList<ArrayList<String>> users = readFile();
+        ArrayList<String> tels = new ArrayList<>();
+        for (List<String> list : users){
+            tels.add(list.get(0));
+        }
+        ArrayList<String> listDelTel = users.get(tels.indexOf(tel));
+        if(listDelTel.get(2).equals("0")){
+            return false;
+        }else{
+            for (int i = 0; i < Integer.parseInt(listDelTel.get(2)); i++) {
+                if (listDelTel.get(3+i).equals(contact)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
